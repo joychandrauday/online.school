3c2b19b10a0f9ce2c5470b19302a9aec3afc5fb4
joychandrauday <joychandraud@gmail.com>
2024-02-04 06:22:05 +0600

    failed to deploy dome manipulation

--- js/main.js
+++ js/main.js
@@ -1,3 +1,8 @@
 //checkout page
 
 // adding items 
+function sendData(add) {
+    let title1=document.getElementById("courseTitle1");
+    let title1=console.log(title1.textContent)
+    document.getElementById("cartCourseTitle1").innerHTML = ($(title1));
+}
